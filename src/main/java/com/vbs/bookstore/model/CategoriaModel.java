7611efac8b5ab2a8c71e92f1b6e1b4e5ab09220a
unknown <brenaairotiv18@gmail.com>
2024-06-20 20:57:47 -0300

    Finalizando as classes dominio

--- src/main/java/com/vbs/bookstore/model/CategoriaModel.java
+++ src/main/java/com/vbs/bookstore/model/CategoriaModel.java
@@ -1,18 +1,26 @@
 package com.vbs.bookstore.model;
 
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode
-public class CategoriaModel {
+@Entity(name = "categoria") //informa hibernate e jpa que a classe categoria é uma entidade, e que pode criar uma tabela no banco de dados
+
+public class CategoriaModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List <LivroModel> livros = new ArrayList<>();
 
     public CategoriaModel(Integer id, String nome, String descricao) {
--- src/main/java/com/vbs/bookstore/model/LivroModel.java
+++ src/main/java/com/vbs/bookstore/model/LivroModel.java
@@ -1,16 +1,25 @@
 package com.vbs.bookstore.model;
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class LivroModel {
+@Entity(name = "livro") //informa hibernate e jpa que a classe categoria é uma entidade, e que pode criar uma tabela no banco de dados
+public class LivroModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nomeAutor;
     private String texto;
 
-    private CategoriaModel categoriaModel;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private CategoriaModel categoria;
 }
