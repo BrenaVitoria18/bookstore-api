1892eacb726d6426d24742ac9a3508bc621e6bdc
unknown <brenaairotiv18@gmail.com>
2024-06-27 19:47:13 -0300

    Ajustes nas validações de erro

--- src/main/java/com/vbs/bookstore/controller/exception/FieldMessage.java
+++ src/main/java/com/vbs/bookstore/controller/exception/FieldMessage.java
@@ -1,17 +1,16 @@
 package com.vbs.bookstore.controller.exception;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class StandardError {
-    private Long timestamp;
-    private Integer status;
-    private String error;
+public class FieldMessage implements Serializable {
 
-
+    private String nomeCampo;
+    private String mensagem;
 }
